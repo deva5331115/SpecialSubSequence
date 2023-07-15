c63d4a3bcd4745219a66639adbf36259b1ed35bc
deva5331115 <devarasangtn@gmail.com>
2023-07-15 14:08:30 +0530

    first commit

---
+++ src/SpecialSubSequence.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SpecialSubSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the word ");
+        String alphabets = scanner.next();
+
+        scanner.close();
+        System.out.println(Counter(alphabets));
+
+    }
+
+    public static int Counter(String alphabets) {
+        int result = 0, a_count = 0;
+        for (int i = 0; i < alphabets.length(); i++) {
+            if ((alphabets.charAt(i) == 'A')) {
+                a_count++;
+                System.out.println(a_count);
+
+            } else if (alphabets.charAt(i) == 'G') {
+                result += a_count;
+                System.out.println("res"+result);
+            }
+        }
+        return result;
+    }
+}
